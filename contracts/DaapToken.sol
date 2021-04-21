b74881f0778b1d11fbd2c76314e53a9e36b5479e
Anurag <anuragvishwacs@gmail.com>
2021-04-21 22:05:37 +0530

    added name,symbol and standard

--- contracts/DaapToken.sol
+++ contracts/DaapToken.sol
@@ -3,11 +3,17 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract DaapToken {
     uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    string public standard;
 
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initalSupply) public {
         balanceOf[msg.sender] = _initalSupply;
         totalSupply = _initalSupply;
+        name = "Triology";
+        symbol = "TL";
+        standard = "TL v1.0";
     }
 }
--- test/DaapToken.js
+++ test/DaapToken.js
@@ -1,14 +1,15 @@
 var DaapToken = artifacts.require("DaapToken");
 
 contract("DaapToken", (accounts) => {
-  let tokenInstance;
-  let totalSupply;
-  let adminBalance;
+  let tokenInstance, totalSupply, adminBalance, name, symbol, standard;
 
   beforeEach(async () => {
     tokenInstance = await DaapToken.deployed();
     totalSupply = await tokenInstance.totalSupply();
     adminBalance = await tokenInstance.balanceOf(accounts[0]);
+    name = await tokenInstance.name();
+    symbol = await tokenInstance.symbol();
+    standard = await tokenInstance.standard();
   });
 
   describe("DaapToken Contract", () => {
@@ -18,6 +19,18 @@ contract("DaapToken", (accounts) => {
 
     it("it allocated the initial supply to admin(account(0)) 1million tokens(100000) ", async () => {
       await assert.strictEqual(adminBalance.toNumber(), 1000000);
+    });
+
+    it("initializes the contract with name", async () => {
+      await assert.strictEqual(name, "Triology");
+    });
+
+    it("initializes the contract with symbol", async () => {
+      await assert.strictEqual(symbol, "TL");
+    });
+
+    it("initializes the contract with standard", async () => {
+      await assert.strictEqual(standard, "TL v1.0");
     });
   });
 });
